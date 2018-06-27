4461d32930f4597c12ffac4c7b39c6445fa8ba10
Agarwal <Kalp.Agarwal@scientificgames.com>
2018-06-27 17:42:29 +0530

    add connect and disconnect handlers

--- server/server.js
+++ server/server.js
@@ -10,6 +10,14 @@ var server = http.createServer(app);
 var io =socketIO(server);
 app.use(express.static(publicPath));
 
+io.on('connection',(socket)=>{
+	console.log('new user connected');
+	socket.on('disconnect',()=>{
+	console.log('new user disconnect');
+});
+});
+
+
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
