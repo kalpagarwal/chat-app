326e39ce6fd08622a30837ba0ea4b15c982afa53
Agarwal <Kalp.Agarwal@scientificgames.com>
2018-06-29 11:58:26 +0530

    add new msg and create msg event

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'jen@example.com',					//sending to server (2)
+    text: 'Hey. This is Andrew.'
+  });
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {   //receiving from server(1)[event listener]
+  console.log('newMessage', message);
+});
--- server/server.js
+++ server/server.js
@@ -2,22 +2,48 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
-
 var server = http.createServer(app);
-var io =socketIO(server);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-io.on('connection',(socket)=>{
-	console.log('new user connected');
-	socket.on('disconnect',()=>{
-	console.log('new user disconnect');
-});
-});
+io.on('connection', (socket) => {
+  console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Kalp',           //sending to client (1)
+    text: 'Hey. What is going on.',
+    createAt: 123
+  });
+
+  socket.on('createMessage', (message) => {      //receiving from client (2)
+    console.log('createMessage', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
 
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
+});
+
+
+
+io.on("connection", function (socket) {
+    var tweet = {user: "nodesource", text: "Hello, world!"};
+
+    // to make things interesting, have it send every second
+    var interval = setInterval(function () {
+        socket.emit("tweet", tweet);
+    }, 1000);
+
+    socket.on("disconnect", function () {
+        clearInterval(interval);
+    });
 });
