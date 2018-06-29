f8665672a30238846632eb0a6d27c8167ef1d16f
Agarwal <Kalp.Agarwal@scientificgames.com>
2018-06-29 12:39:40 +0530

    featchers added

--- server/server.js
+++ server/server.js
@@ -19,6 +19,17 @@ io.on('connection', (socket) => {
   //   text: 'Hey. What is going on.',
   //   createAt: 123
   // });
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (message) => {      //receiving from client (2)
     console.log('createMessage', message);
@@ -27,6 +38,11 @@ io.on('connection', (socket) => {
       text:message.text,
       createAt:new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage',{
+    //   from:message.from,
+    //   text:message.text,
+    //   createdAt:new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
