6334b86d27b788d9f3f16127187eb93943624cc1
Agarwal <Kalp.Agarwal@scientificgames.com>
2018-07-02 18:06:59 +0530

    added login page

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,17 @@
 var socket = io();
 
 socket.on('connect', function () {
+  var params = jQuery.deparam(window.location.search);
+  socket.emit('join',params, function(err){
+        if(err)
+        {
+          alert(err)
+          window.location.href='/';
+        }
+        else{
+          console.log('no error');
+        }
+  });
   console.log('Connected to server');
 });
 
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const{isRealString} = require('./utils/validation');
 const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -18,7 +19,14 @@ io.on('connection', (socket) => {
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
-
+  socket.on('join',(params,callback)=>{
+      if(!isRealString(params.name)|| !isRealString(params.room)){
+        callback('Name and Room is required');
+      }
+      else{
+        
+      }
+  });
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
